1f85d3d20507ea33ff80c00e7db2b05af56fc778
areenaLn <areena@aquilastudio.net>
2022-08-29 09:40:00 +0700

    enitPath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     AUTH_TOKEN_NAME: "auth",
     AUTH_TOKEN_USER: "user",
-    URL_API: "http://172.20.10.11/UngPhp4",
+    URL_API: "http://ect.pn.psu.ac.th/tidd/UngPhp4",
     COOKIE_PAMENT: "state_payment",
 };
